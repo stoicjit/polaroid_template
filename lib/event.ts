f11e1e2fcd67911476322afb96591185a2e386e3
stoicjit <125784882+stoicjit@users.noreply.github.com>
2026-06-25 22:58:32 -0400

    deploy

--- lib/event.ts
+++ lib/event.ts
@@ -7,7 +7,7 @@ export const eventConfig = {
   welcomeBackground: "/photos/photo6.jpg",
   welcomePhotos: [
     { src: "/photos/photo1.jpg", rotate: "-8deg", top: "2%", left: "-8%" },
-    { src: "/photos/photo2.png", rotate: "9deg", top: "1%", right: "-9%" },
+    { src: "/photos/photo4.png", rotate: "9deg", top: "1%", right: "-9%" },
     { src: "/photos/photo3.jpg", rotate: "-5deg", top: "35%", left: "-8%" },
     { src: "/photos/photo4.png", rotate: "5deg", top: "33%", right: "-8%" },
     { src: "/photos/photo1.jpg", rotate: "8deg", bottom: "5%", left: "-7%" },
