5feac882dcfe06789b8c9c5d0ba551dc2e61d06d
stoicjit <125784882+stoicjit@users.noreply.github.com>
2026-06-25 23:32:06 -0400

    couple photos added

--- lib/event.ts
+++ lib/event.ts
@@ -6,11 +6,11 @@ export const eventConfig = {
   dateLabel: "04.07.2026",
   welcomeBackground: "/photos/photo6.jpg",
   welcomePhotos: [
-    { src: "/photos/photo1.jpg", rotate: "-8deg", top: "2%", left: "-8%" },
-    { src: "/photos/photo4.png", rotate: "9deg", top: "1%", right: "-9%" },
-    { src: "/photos/photo3.jpg", rotate: "-5deg", top: "35%", left: "-8%" },
-    { src: "/photos/photo4.png", rotate: "5deg", top: "33%", right: "-8%" },
-    { src: "/photos/photo1.jpg", rotate: "8deg", bottom: "5%", left: "-7%" },
-    { src: "/photos/photo2.png", rotate: "-8deg", bottom: "6%", right: "-6%" },
+    { src: "/photos/beach2.jpeg", rotate: "-8deg", top: "2%", left: "-8%" },
+    { src: "/photos/pink.jpeg", rotate: "9deg", top: "1%", right: "-9%" },
+    { src: "/photos/manja.png", rotate: "-5deg", top: "35%", left: "-8%" },
+    { src: "/photos/kids2.jpeg", rotate: "5deg", top: "33%", right: "-8%" },
+    { src: "/photos/class.jpeg", rotate: "8deg", bottom: "5%", left: "-7%" },
+    { src: "/photos/christmas.jpeg", rotate: "-8deg", bottom: "6%", right: "-6%" },
   ],
 } as const;
